3f66b517ab817b30e542e58f0f26ba04fa764885
syafiqsny-debug <syafiqsny@gmail.com>
2026-06-08 09:32:07 +0800

    Update project.py

--- project.py
+++ project.py
@@ -15,7 +15,7 @@ DF.dropna(inplace = True)
 DF.duplicated().sum()
 DF.drop_duplicates(inplace = True)
 DF = DF[DF['year'] >= 2019]
-DF = DF.rename(columns={'model': 'Model','year' : 'Year', 'price':'Price', 'transmission':'Transmission', 'mileage':'Mileage'})
+DF = DF.rename(columns={'model': 'Model','year' : 'Year', 'price':'Price', 'transmission':'Transmission', 'mileage':'Mileage', 'mpg':'MilesperGallon'})
 DF = DF[DF['fuelType']== 'Petrol']
 
 
@@ -23,18 +23,17 @@ DF = DF[DF['fuelType']== 'Petrol']
 
 st.subheader("Histogram")
 
-numeric_columns = DF.select_dtypes(include=['int64', 'float64']).columns.tolist()
-column = st.selectbox("Choose a numeric column for the histogram", numeric_columns, key="hist_column_select")
+fig, ax = plt.subplots(figsize=(10, 6))
 
-fig_plotly = px.histogram(DF, x=column, title=f"Plotly Distribution of {column}")
-fig_plotly.update_traces(marker={"color": "purple", "line": {"color": "black", "width": 2}})
+DF['MilesperGallon'].plot(kind='hist', 
+                    ax=ax)
 
-# Fix the x-axis range based on the minimum and maximum values of the selected column
-fig_plotly.update_layout(
-    xaxis_range=[DF[column].min(), DF[column].max()]
-)
+plt.title('Histogram Plot')
 
-st.plotly_chart(fig_plotly)
+plt.xlabel('MilesperGallon')
+plt.ylabel('Frequency')
+plt.show()
+
 
 st.subheader("Scatter Chart")
 x_column = st.selectbox("Choose x-axis column",DF.columns)
